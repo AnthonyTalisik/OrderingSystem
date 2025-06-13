9447c7108fa13759e33cd727f02a8893207bb2f3
Talisik <talisikninoanthony@gmail.com>
2025-06-13 16:33:10 +0800

    Function for the different menu

--- src/main/java/com/example/orderingsystem/HelloController.java
+++ src/main/java/com/example/orderingsystem/HelloController.java
@@ -1,14 +1,48 @@
 package com.example.orderingsystem;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void handleBurgerClick(javafx.scene.input.MouseEvent mouseEvent) {
+        try {
+            Parent burgerRoot = FXMLLoader.load(getClass().getResource("Burgers.fxml"));
+            Scene scene = ((Node) mouseEvent.getSource()).getScene();
+            scene.setRoot(burgerRoot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleBreakfastClick(MouseEvent mouseEvent) {
+        try {
+            Parent burgerRoot = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            Scene scene = ((Node) mouseEvent.getSource()).getScene();
+            scene.setRoot(burgerRoot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void handleSpagClick(MouseEvent mouseEvent) {
+        try {
+            Parent burgerRoot = FXMLLoader.load(getClass().getResource("Spag.fxml"));
+            Scene scene = ((Node) mouseEvent.getSource()).getScene();
+            scene.setRoot(burgerRoot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.orderingsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.orderingsystem to javafx.fxml;
